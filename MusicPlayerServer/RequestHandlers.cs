345a39d374882a4e64d66d5a61eed10698f0b03a
Mario Morcos <mariomorcoswassily@gmail.com>
2023-11-26 19:34:11 +0200

    fixed database context error in RequestHandlers

--- MusicPlayerServer/RequestHandlers.cs
+++ MusicPlayerServer/RequestHandlers.cs
@@ -9,95 +9,82 @@ namespace MusicPlayerServer
     {
         public static async Task<IResult> GetLikedSongs()
         {
-            IQueryable query;
-            await using (var context = new MusicPlayerServerContext())
-            {
-                query = from userLikes
-                        in context.UserLikes
-                        where userLikes.UserID == 1
-                        select new 
-                        { 
-                            userLikes.SongID ,
-                            userLikes.Song.Name 
-                        };
-            }
+            var context = new MusicPlayerServerContext();
+            var query = from userLikes
+                    in context.UserLikes
+                    where userLikes.UserID == 1
+                    select new 
+                    { 
+                        userLikes.SongID ,
+                        userLikes.Song.Name 
+                    };
             return Results.Ok(query);
         }
 
         public static async Task<IResult> GetOwnSongs()
         {
-            IQueryable query;
-            await using(var context = new MusicPlayerServerContext())
-            {
-                query = from songs
-                        in context.Songs
-                        where songs.UserID == 1
-                        select new
-                        {
-                            songs.SongID,
-                            songs.Name
-                        };
-            }
+            
+            var context = new MusicPlayerServerContext();
+            var query = from songs
+                    in context.Songs
+                    where songs.UserID == 1
+                    select new
+                    {
+                        songs.SongID,
+                        songs.Name
+                    };
             return Results.Ok(query);
         }
 
         public static async Task<IResult> GetAllSongs()
         {
-            IQueryable query;
-            await using(var context = new MusicPlayerServerContext())
-            {
-                query = from songs
-                        in context.Songs
-                        select new
-                        {
-                            songs.SongID,
-                            songs.Name
-                        };
-            }
+            var context = new MusicPlayerServerContext();
+            var query = from songs
+                    in context.Songs
+                    select new
+                    {
+                        songs.SongID,
+                        songs.Name
+                    };
             return Results.Ok(query);
         }
 
         public static async Task<IResult> Search(string songName)
         {
             string[] songKeyWords = songName.Split(" ");
-            IQueryable query;
-            await using(var context = new MusicPlayerServerContext())
-            {
-                query = from songs
-                        in context.Songs
-                        where songKeyWords.Contains(songName)
-                        select new
-                        {
-                            songs.SongID,
-                            songs.Name
-                        };
-            }
+            var context = new MusicPlayerServerContext();
+            var query = from songs
+                    in context.Songs
+                    where songKeyWords.Contains(songName)
+                    select new
+                    {
+                        songs.SongID,
+                        songs.Name
+                    };
             return Results.Ok(query);
         }
 
         public static async Task<IResult> GetAllPlaylists()
         {
-            IQueryable query;
-            await using(var context = new MusicPlayerServerContext())
-            {
-                query = from playlist
-                        in context.Playlists
-                        where playlist.UserID == 1
-                        select new
-                        {
-                            playlist.PlaylistID,
-                            playlist.Name
-                        };
-            }
+            var context = new MusicPlayerServerContext();
+
+            var query = from playlist
+                    in context.Playlists
+                    where playlist.UserID == 1
+                    select new
+                    {
+                        playlist.PlaylistID,
+                        playlist.Name
+                    };
+
             return Results.Ok(query);
         }
 
         public static async Task<IResult> GetPlayListSongs(int playlistID)
         {
-            IQueryable query;
-            await using(var context = new MusicPlayerServerContext())
-            {
-                query = from songPlaylist
+            var context = new MusicPlayerServerContext();
+
+            var query = from songPlaylist
                         in context.SongPlaylists
                         where songPlaylist.PlaylistID == playlistID
                         select new
@@ -105,75 +92,75 @@ namespace MusicPlayerServer
                             songPlaylist.SongID,
                             songPlaylist.Song.Name
                         };
-            }
+
             return Results.Ok(query);
         }
 
         public static async Task<IResult> AddSong(Song song)
         {
-            await using(var context = new MusicPlayerServerContext())
-            {
-                await context.Songs.AddAsync(song);
-                context.SaveChanges();
-            }
+            var context = new MusicPlayerServerContext();
+
+            context.Songs.AddAsync(song);
+            context.SaveChanges();
+
             return Results.Ok();
         }
 
         public static async Task<IResult> AddPlaylist(Playlist playlist)
         {
-            await using(var context = new MusicPlayerServerContext())
-            {
-                await context.Playlists.AddAsync(playlist);
-                context.SaveChanges();
-            }
+            var context = new MusicPlayerServerContext();
+
+            await context.Playlists.AddAsync(playlist);
+            context.SaveChanges();
+
             return Results.Ok();
         }
 
         public static async Task<IResult> AddSongToPlaylist(SongPlaylist songPlaylist)
         {
-            await using(var context = new MusicPlayerServerContext())
-            {
-                await context.SongPlaylists.AddAsync(songPlaylist);
-                context.SaveChanges();
-            }
+            var context = new MusicPlayerServerContext();
+
+            await context.SongPlaylists.AddAsync(songPlaylist);
+            context.SaveChanges();
+
             return Results.Ok();
         }
 
         public static async Task<IResult> DeleteSong(int songID)
         {
-            await using(var context = new MusicPlayerServerContext())
-            {
-                var query = from song
-                            in context.Songs
-                            where song.SongID == songID
-                            select song;
-                context.Songs.Remove(query.FirstOrDefault());
-                context.SaveChanges();
-            }
+            var context = new MusicPlayerServerContext();
+
+            var query = from song
+                        in context.Songs
+                        where song.SongID == songID
+                        select song;
+            context.Songs.Remove(query.FirstOrDefault());
+            context.SaveChanges();
+
             return Results.Ok();
         }
 
         public static async Task<IResult> AddLikeToSong(int songID)
         {
-            await using(var context = new MusicPlayerServerContext())
-            {
-                UserLikes entry = new UserLikes { SongID = songID, UserID = 1 };
-                context.UserLikes.Add(entry);
-                context.SaveChanges();
-            }
+            var context = new MusicPlayerServerContext();
+
+            UserLikes entry = new UserLikes { SongID = songID, UserID = 1 };
+            context.UserLikes.Add(entry);
+            context.SaveChanges();
+
             return Results.Ok();
         }
 
         public static async Task<IResult> GetSong(int songID)
         {
-            IQueryable query;
-            await using(var context = new MusicPlayerServerContext())
-            {
-                query = from song
-                        in context.Songs
-                        where song.SongID == songID
-                        select song;
-            }
+
+            var context = new MusicPlayerServerContext();
+
+            var query = from song
+                    in context.Songs
+                    where song.SongID == songID
+                    select song;
+
             return Results.Ok(query);
         }
 
