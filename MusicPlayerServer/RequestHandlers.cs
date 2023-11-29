341aa5a6dc5fc1c41d0e4392205ee3e795836161
Mario Morcos <mariomorcoswassily@gmail.com>
2023-11-29 11:33:16 +0200

    added extra authorization in RequestHandlers

--- MusicPlayerServer/RequestHandlers.cs
+++ MusicPlayerServer/RequestHandlers.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using MusicPlayerServer.Models;
 using NuGet.Protocol;
+using System.Net.Http;
 using System.Security.Cryptography.Xml;
 
 namespace MusicPlayerServer
 {
+    public record AddSongRecord(string name, byte[] file, byte[] picture);
+    public record AddPlaylistRecord(string name);
     public class RequestHandlers
     {
         private static MusicPlayerServerContext context = new MusicPlayerServerContext();
-        public static async Task<IResult> GetLikedSongs()
+        public static async Task<IResult> GetLikedSongs(HttpContext httpContext)
         {
-            //var context = new MusicPlayerServerContext();
+           
+            int userID = Convert.ToInt32(Authorization.GetCookie("userID", httpContext));
             var query = from userLikes
                         in context.UserLikes
-                        where userLikes.UserID == 1
+                        where userLikes.UserID == userID
                         select new 
                         { 
                             userLikes.SongID ,
@@ -22,13 +27,14 @@ namespace MusicPlayerServer
             return Results.Ok(query);
         }
 
-        public static async Task<IResult> GetOwnSongs()
+        public static async Task<IResult> GetOwnSongs(HttpContext httpContext)
         {
-            
-            //var context = new MusicPlayerServerContext();
+
+       
+            int userID = Convert.ToInt32(Authorization.GetCookie("userID", httpContext));
             var query = from songs
                     in context.Songs
-                    where songs.UserID == 1
+                    where songs.UserID == userID
                     select new
                     {
                         songs.SongID,
@@ -39,7 +45,7 @@ namespace MusicPlayerServer
 
         public static async Task<IResult> GetAllSongs()
         {
-            //var context = new MusicPlayerServerContext();
+        
             var query = from songs
                     in context.Songs
                     select new
@@ -52,8 +58,8 @@ namespace MusicPlayerServer
 
         public static async Task<IResult> Search(string songName)
         {
-            string[] songKeyWords = songName.Split(" ");
-            //var context = new MusicPlayerServerContext();
+            string[] songKeyWords = songName.Split("_");
+            
             var query = from songs
                     in context.Songs
                     where songKeyWords.Contains(songName)
@@ -65,13 +71,14 @@ namespace MusicPlayerServer
             return Results.Ok(query);
         }
 
-        public static async Task<IResult> GetAllPlaylists()
+        public static async Task<IResult> GetAllPlaylists(HttpContext httpContext)
         {
-            //var context = new MusicPlayerServerContext();
+            
+            int userID = Convert.ToInt32(Authorization.GetCookie("userID", httpContext));
 
             var query = from playlist
                     in context.Playlists
-                    where playlist.UserID == 1
+                    where playlist.UserID == userID
                     select new
                     {
                         playlist.PlaylistID,
@@ -81,13 +88,14 @@ namespace MusicPlayerServer
             return Results.Ok(query);
         }
 
-        public static async Task<IResult> GetPlayListSongs(int playlistID)
+        public static async Task<IResult> GetPlayListSongs(int playlistID, HttpContext httpContext)
         {
-            //var context = new MusicPlayerServerContext();
+            
+            int userID = Convert.ToInt32(Authorization.GetCookie("userID", httpContext));
 
             var query = from songPlaylist
                         in context.SongPlaylists
-                        where songPlaylist.PlaylistID == playlistID
+                        where songPlaylist.PlaylistID == playlistID && songPlaylist.Playlist.UserID == userID
                         select new
                         {
                             songPlaylist.SongID,
@@ -97,19 +105,24 @@ namespace MusicPlayerServer
             return Results.Ok(query);
         }
 
-        public static async Task<IResult> AddSong(Song song)
+        public static async Task<IResult> AddSong(AddSongRecord songToAdd, HttpContext httpContext)
         {
-            //var context = new MusicPlayerServerContext();
-            song.Likes = 0;
-            context.Songs.AddAsync(song);
+      
+            int userID = Convert.ToInt32(Authorization.GetCookie("userID", httpContext));
+            
+            Song song = new Song { Name = songToAdd.name, File = songToAdd.file, Likes = 0, Picture = songToAdd.picture, UserID = userID };
+            
+            await context.Songs.AddAsync(song);
             context.SaveChanges();
 
             return Results.Ok();
         }
 
-        public static async Task<IResult> AddPlaylist(Playlist playlist)
+        public static async Task<IResult> AddPlaylist(AddPlaylistRecord playlistToAdd, HttpContext httpContext)
         {
-            //var context = new MusicPlayerServerContext();
+
+            int userID = Convert.ToInt32(Authorization.GetCookie("userID", httpContext));
+            Playlist playlist = new Playlist { Name = playlistToAdd.name, UserID = userID };
 
             await context.Playlists.AddAsync(playlist);
             context.SaveChanges();
@@ -119,7 +132,7 @@ namespace MusicPlayerServer
 
         public static async Task<IResult> AddSongToPlaylist(SongPlaylist songPlaylist)
         {
-            //var context = new MusicPlayerServerContext();
+
 
             await context.SongPlaylists.AddAsync(songPlaylist);
             context.SaveChanges();
@@ -127,13 +140,14 @@ namespace MusicPlayerServer
             return Results.Ok();
         }
 
-        public static async Task<IResult> DeleteSong(int songID)
+        public static async Task<IResult> DeleteSong(int songID, HttpContext httpContext)
         {
-            //var context = new MusicPlayerServerContext();
+
+            int userID = Convert.ToInt32(Authorization.GetCookie("userID", httpContext));
 
             var query = from song
                         in context.Songs
-                        where song.SongID == songID
+                        where song.SongID == songID && song.UserID == userID
                         select song;
             context.Songs.Remove(query.FirstOrDefault());
             context.SaveChanges();
@@ -141,12 +155,13 @@ namespace MusicPlayerServer
             return Results.Ok();
         }
 
-        public static async Task<IResult> AddLikeToSong(int songID)
+        public static async Task<IResult> AddLikeToSong(int songID, HttpContext httpContext)
         {
-            //var context = new MusicPlayerServerContext();
 
-            UserLikes entry = new UserLikes { SongID = songID, UserID = 1 };
-            context.UserLikes.Add(entry);
+            int userID = Convert.ToInt32(Authorization.GetCookie("userID", httpContext));
+
+            UserLikes entry = new UserLikes { SongID = songID, UserID = userID };
+            await context.UserLikes.AddAsync(entry);
             context.SaveChanges();
 
             return Results.Ok();
@@ -155,7 +170,6 @@ namespace MusicPlayerServer
         public static async Task<IResult> GetSong(int songID)
         {
 
-            //var context = new MusicPlayerServerContext();
 
             var query = from song
                     in context.Songs
